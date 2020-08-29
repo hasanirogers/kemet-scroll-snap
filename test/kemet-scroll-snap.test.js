2f8308dcff9b474f81372735a34b3e44cc60b767
Hasani Rogers <hasani.rogers@gmail.com>
2020-08-28 23:08:46 -0400

    added unit tests

--- test/kemet-scroll-snap.test.js
+++ test/kemet-scroll-snap.test.js
@@ -3,30 +3,47 @@ import { html, fixture, expect } from '@open-wc/testing';
 import '../kemet-scroll-snap.js';
 
 describe('KemetScrollSnap', () => {
-  it('has a default title "Hey there" and counter 5', async () => {
-    const el = await fixture(html`
-      <kemet-scroll-snap></kemet-scroll-snap>
+  it('has the proper default attributes', async () => {
+    const element = await fixture(html`
+      <kemet-scroll-snap>
+        <div slot="slides">
+          <kemet-scroll-snap-slide>[your content here]</kemet-scroll-snap-slide>
+          <kemet-scroll-snap-slide>[your content here]</kemet-scroll-snap-slide>
+          <kemet-scroll-snap-slide>[your content here]</kemet-scroll-snap-slide>
+        </div>
+      </kemet-scroll-snap>
     `);
 
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
+    expect(element.axis).to.equal('horizontal');
+    expect(element.pagination).to.equal('bottom');
+    expect(element.paginationIcon).to.equal('•');
   });
 
-  it('increases the counter on button click', async () => {
-    const el = await fixture(html`
-      <kemet-scroll-snap></kemet-scroll-snap>
+  it('sets the vertical html attribute while axis is vertical', async () => {
+    const element = await fixture(html`
+      <kemet-scroll-snap axis="vertical"></kemet-scroll-snap>
     `);
-    el.shadowRoot.querySelector('button').click();
 
-    expect(el.counter).to.equal(6);
+    const htmlElement = document.querySelector('html');
+
+    element.setVerticalAttribute();
+    expect(htmlElement.getAttribute('kemet-scroll-snap-axis')).to.equal('vertical');
   });
 
-  it('can override the title via attribute', async () => {
-    const el = await fixture(html`
-      <kemet-scroll-snap title="attribute title"></kemet-scroll-snap>
+  it('successfully populates slide data when slides are present', async () => {
+    const element = await fixture(html`
+      <kemet-scroll-snap>
+        <div slot="slides">
+          <kemet-scroll-snap-slide>[your content here]</kemet-scroll-snap-slide>
+          <kemet-scroll-snap-slide>[your content here]</kemet-scroll-snap-slide>
+          <kemet-scroll-snap-slide>[your content here]</kemet-scroll-snap-slide>
+        </div>
+      </kemet-scroll-snap>
     `);
 
-    expect(el.title).to.equal('attribute title');
+    element.makeSlides();
+
+    expect(element.slides[0].id).to.equal(0);
   });
 
   it('passes the a11y audit', async () => {
